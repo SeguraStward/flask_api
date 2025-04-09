58a778fac7d46d9d49aff00509a7920e60d50ead
SeguraStward <stwardsegura239@gmail.com>
2025-04-08 21:38:13 -0600

    feature: cors

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-
+CORS(app)
 # URL de la API RandomUser.me
 API_URL = "https://randomuser.me/api/"
 
 # Endpoint para devolver los usuarios generados
-@app.route("/users", methods=["GET"])
+@app.route("/api/users", methods=["GET"])
 def get_users():
     try:
         # Realizar solicitud para obtener 75 usuarios aleatorios de la API
